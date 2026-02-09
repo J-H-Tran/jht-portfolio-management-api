4aa6c643b2e1504b679031d4ecd89c6b93dc8ec6
J-H-Tran <tr.h.jonathan@gmail.com>
2026-02-10 01:44:03 +0900

    cleanup

--- src/main/java/com/pgim/portfolio/PortfolioApiApplication.java
+++ src/main/java/com/pgim/portfolio/PortfolioApiApplication.java
@@ -1,24 +1,14 @@
 package com.pgim.portfolio;
 
-import com.pgim.portfolio.service.TradeService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
 @SpringBootApplication(scanBasePackages = "com.pgim.portfolio")
 @EnableJpaRepositories(basePackages = "com.pgim.portfolio.repository")
-@EnableSpringDataWebSupport//(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
-public class PortfolioApiApplication /*implements CommandLineRunner*/ {
-    private final TradeService tradeService;
-
-    public PortfolioApiApplication(TradeService tradeService) {
-        this.tradeService = tradeService;
-    }
-
+@EnableSpringDataWebSupport
+public class PortfolioApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApiApplication.class, args);
     }
