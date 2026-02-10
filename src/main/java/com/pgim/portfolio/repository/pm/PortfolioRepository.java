aaf102a03ddc4a5d1b314416436dc02d674d8051
J-H-Tran <tr.h.jonathan@gmail.com>
2026-02-10 21:11:50 +0900

    address lazy loading using join fetch

--- src/main/java/com/pgim/portfolio/repository/pm/PortfolioRepository.java
+++ src/main/java/com/pgim/portfolio/repository/pm/PortfolioRepository.java
@@ -2,6 +2,16 @@ package com.pgim.portfolio.repository.pm;
 
 import com.pgim.portfolio.domain.entity.pm.Portfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+    @Query("SELECT p FROM Portfolio p LEFT JOIN FETCH p.trades")
+    List<Portfolio> findAllWithTrades();
+
+    @Query("select p from Portfolio p left join fetch p.trades where p.id = :id")
+    Optional<Portfolio> findByIdWithTrades(@Param("id") Long id);
 }
